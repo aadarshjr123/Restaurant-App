9529ca95f8ff06f153946d4733b94df1ac877089
Aadarsh Jr <55702370+aadarshjr123@users.noreply.github.com>
2020-04-21 11:22:48 +0530

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -6,6 +6,6 @@ export default axios.create({
     baseURL: 'https://api.yelp.com/v3/businesses',
     headers: {
         Authorization: 
-            'Bearer 0NZzxJUSk9Dupi5S_oZEnCK3W4JnPVjJNKbt-Ws8Env9uV6BdRMhRVis9ak3ebRsCHgAO0Ol5dhzSSPa67k170CljzljCRiom6CJBexjIIz2JcYizZ3KNq0vo7RzXnYx'
+            'Bearer Key'
     }
 });
